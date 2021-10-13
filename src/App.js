a91f82b50a5ac37cd2ab0c33b8d7ed0684756b03
Harshit Bangar <harshitbangar@Harshits-MacBook-Pro.local>
2021-10-13 20:08:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          bye there mowgly!
+          Edifdft <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
