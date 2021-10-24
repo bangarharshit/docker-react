0c567f83d48bc13d27ea9f1f002945c1a69acf11
Harshit Bangar <harshitbangar@Harshits-MacBook-Pro.local>
2021-10-24 09:10:30 +0530

    made some change in feature

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+        I was changed on the feature branch
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
